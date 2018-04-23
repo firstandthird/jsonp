9fbaade6553b9d5c22dc0378107039a7507ec494
Alejandro Pérez Martín <AlejandroPerezMartin@users.noreply.github.com>
2018-04-23 17:04:34 +0200

    Fix invalid generated URL if no params in source URL (#1)
    * Fix invalid generated URL if no params in source URL
    
    * Add tests for source URL

--- test/jsonp.test.js
+++ test/jsonp.test.js
@@ -9,6 +9,7 @@ const getScripts = (url) => [].slice.call(document.querySelectorAll('script'))
   .map(script => script.src);
 
 const testURL = 'test/quickjsonp.test.js';
+const testParamsURL = 'test/quickjsonp.test.js?param=true';
 
 test('jsonp', assert => {
   let called = false;
@@ -21,8 +22,27 @@ test('jsonp', assert => {
 
   const callbackName = getCallbackName();
   const script = getScripts(testURL)[0];
-  const expectedScript = `${testURL}&callback=${callbackName[0]}`;
+  const expectedScript = `${testURL}?callback=${callbackName[0]}`;
   assert.ok(script.indexOf(expectedScript) > -1, 'forms a valid jsonp request');
 
   window[callbackName]();
 });
+
+test('jsonp - URL without params', assert => {
+  jsonp(testURL, () => {});
+
+  const script = getScripts(testURL)[0];
+  const expectedScript = `${testURL}?callback=`;
+  assert.ok(script.indexOf(expectedScript) > -1, 'forms a valid jsonp request');
+  assert.end();
+});
+
+test('jsonp - URL with params', assert => {
+  jsonp(testParamsURL, () => {});
+
+  const script = getScripts(testParamsURL)[0];
+  const expectedScript = `${testParamsURL}&callback=`;
+  assert.ok(script.indexOf(expectedScript) > -1, 'forms a valid jsonp request');
+  assert.end();
+});
+
